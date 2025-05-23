3363b70bef629979ebf9e4e91724f1c280528829
leah506 <itsleahnjoki@gmail.com>
2025-05-23 19:07:56 +0300

    Add files via upload

---
+++ function.py
@@ -0,0 +1,20 @@
+def rectangle(x):
+    p = x + x + x + x
+    return p
+
+def perimeter(side):
+    rectangle(side)
+    print("perimeter of a rectangle", rectangle(side))
+
+perimeter(6)
+
+
+
+
+
+
+
+
+    
+
+
