03d10c90e844e5cbff518806943351e44abb9777
leah506 <itsleahnjoki@gmail.com>
2025-05-24 15:49:42 +0300

    Update calls.py

---
+++ calls.py
@@ -0,0 +1,13 @@
+from function import student, age, perimeter, rectangle
+student
+age(2006)
+perimeter(5)
+rectangle(4)
+
+
+
+
+from classes import hospital, animal, library
+hospital()
+animal(type)
+library()
