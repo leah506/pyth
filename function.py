f76853a0b649396198ed93d6457e2e29f61bf548
leah506 <itsleahnjoki@gmail.com>
2025-05-24 15:51:14 +0300

    Update function.py

--- function.py
+++ function.py
@@ -10,6 +10,29 @@ perimeter(6)
 
 
 
+def age(yob):
+    year = 2025 - yob
+    
+
+
+
+
+
+def student ():
+    a = input("enter your name")
+    level= input("enter your level(high, low)")
+
+
+    if level == "high":
+        print("you are a senior student")
+
+    elif level == "low":
+        print("you are a junior student")
+    else:
+        print("you are no longer a student here")
+
+
+
 
 
 
